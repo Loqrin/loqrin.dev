e279f4cf3acf4f59e0a2dafe1d942ab922bf6d2b
Loqrin <loqrin@gmail.com>
2023-08-19 21:05:42 +0200

    refactor(site): file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: "jit",
-    purge: ["./dist/*.{html,js}"],
+    purge: ["./*.{html,js}"],
     content: [],
     theme: {
         extend: {
