4bcae3ac33f49146fc86712af82d673c57a4b48b
Loqrin <loqrin@gmail.com>
2023-08-19 20:59:21 +0200

    feat(site): initial tailwind design

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: "jit",
+    purge: ["./dist/*.{html,js}"],
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            backgroundImage: {
+                dots: "url('./assets/dot-grid.webp')",
+                sowhite: "url('./assets/so-white.png')",
+            },
+        },
+    },
+    plugins: [],
+};
