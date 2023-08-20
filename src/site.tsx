618deff98e2a6898352fee04577675b446850709
Loqrin <loqrin@gmail.com>
2023-08-21 01:10:45 +0200

    fix(site): don't use tailwind for background images

--- src/site.tsx
+++ src/site.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Navbar from "./components/navbar";
 import "./css/tailwind.css";
+import "./css/backgrounds.css";
 import Home from "./components/home";
 
 const Site = () => {
     const [navbarRef] = useState<NavbarRef>({});
 
     return (
-        <div className="bg-homesmooth bg-no-repeat bg-cover w-screen h-screen">
+        <div
+            className={`background-homesmooth bg-no-repeat bg-cover w-screen h-screen`}
+        >
             <Navbar pNavbarRef={navbarRef} />
             <Home />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    let extType = assetInfo.name.split(".").at(1);
-                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                        extType = "img";
-                    }
-                    return `[name][extname]`;
-                },
-            },
-        },
-    },
     theme: {
         extend: {
             fontFamily: {
                 poppins: ["Poppins", "sans-serif"],
-            },
-            backgroundImage: {
-                homenoise: "url('./assets/bg_home_noise.png')",
-                homesmooth: "url('./assets/bg_home_smooth.png')",
             },
         },
     },
