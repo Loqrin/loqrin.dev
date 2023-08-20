73089f7961bdbd29e780b5b4d10174efeb7ac6f0
Loqrin <loqrin@gmail.com>
2023-08-20 14:17:18 +0200

    feat(site): vite + react + tailwind setup

--- src/components/defaultComponent.tsx
+++ src/components/defaultComponent.tsx
@@ -1,9 +1,11 @@
-import "../css/defaultComponent.css";
+import "../css/tailwind.css";
 
 const DefaultComponent = () => {
     return (
         <div>
-            <p className="defaultComponent">Vite + React | Foxxo</p>
+            <p className="text-3xl font-bold underline text-orange-400">
+                Vite + React + Tailwind | Foxxo
+            </p>
         </div>
     );
 };
