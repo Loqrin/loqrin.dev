e120f788b7eccb043ae2321f20ff38e3526262ac
Loqrin <loqrin@gmail.com>
2023-08-20 13:00:48 +0200

    refactor(site): remove header state

--- src/components/defaultComponent.tsx
+++ src/components/defaultComponent.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "../css/defaultComponent.css";
 
 const DefaultComponent = () => {
-    const [header, setHeader] = useState("Default Component");
-
     return (
         <div>
-            <p className="defaultComponent">{header}</p>
+            <p className="defaultComponent">Vite + React on GitHub Pages Test</p>
         </div>
     );
 };
