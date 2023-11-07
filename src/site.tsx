609758e3c6f6155057fa39b801dd5f7d7ed06e29
Loqrin <loqrin@gmail.com>
2023-11-07 17:13:05 +0200

    fix(site): change to hash router

--- src/site.tsx
+++ src/site.tsx
@@ -1,7 +1,7 @@
 import "./css/tailwind.css";
 import "./css/backgrounds.css";
 import "./css/scrollbar.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./mainPage";
 import LeToucanPage from "./leToucanPage";
 
